03fcdb7d76c2b1e9bd7e0d4cd090873f1042939a
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-03-03 22:56:27 +0000

    changed py file

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,27 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Jayden Okafor
+Created on: Feb 2026
+This program shows you more cookies when the "a" button is clicked and reset cookies when the "b" button is clicked
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+cookies = 0
+
+while True:
+    if button_a.was_pressed():
+        display.clear()
+        cookies = cookies + 1
+
+        if cookies < 10:
+            display.show(str(cookies))
+        else:
+            display.scroll(str(cookies))
+
+    if button_b.was_pressed():
+        display.clear()
+        cookies = 0
+        display.show(str(cookies))
